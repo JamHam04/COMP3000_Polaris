e46a131477469a1db96612649e25c5fef762dee0
JamHam04 <james.hampson2004@gmail.com>
2025-11-12 23:22:51 +0000

    Controller Raycast Input and Direction
    Check to see which gridObject the hand is facing towards, getting the controllers current facing direction when the trigger is pressed to move the gridObject in the correct direction

--- PolarisVR/Assets/Scripts/GrdObjectInput.cs
+++ PolarisVR/Assets/Scripts/GrdObjectInput.cs
@@ -2,37 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
 
 public class GrdObjectInput : MonoBehaviour
 {
     
     private GridObject gridObject;
-    public ActionBasedController xrController;
-    
+
+
 
     // XR Toolkit button input
-    public InputHelpers.Button magnetButton = InputHelpers.Button.Trigger; // Button to activate magnet
+    public ActionBasedController xrController;
+    public InputActionProperty magnetAction; // Set activate value
+
     public float activationThreshold = 0.1f; // Threshold for button press
 
     // Raycast
     public float raycastDistance = 10.0f; // To be used for magnet target
+    public Transform rayOrigin;
+   
+
+
+    // Left or right hand controller
+    public bool isLeftHand = false;
+    public bool isRightHand = false;
+
+    // 
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        // Get GridController script
-        gridObject = FindObjectOfType<GridObject>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         // Movement pc test input 
-        testInput();
+        //testInput();
 
         // VR input for magnet push/pull
         activateMagnetInput();
+
+        //Debug.Log(xrController.transform.forward);
+
     }
 
     void testInput()
@@ -76,15 +91,74 @@ public class GrdObjectInput : MonoBehaviour
     void activateMagnetInput()
     {
 
+        bool isMagnetActivated = false;
+        // Check if magnet button is pressed
+
+        float magnetValue = magnetAction.action.ReadValue<float>();
+        if (magnetValue > activationThreshold)
+        {
+            isMagnetActivated = true;
+        }
+        
+
+        // Cast ray from controller
+        RaycastHit hit;
+        if (Physics.Raycast(rayOrigin.position, rayOrigin.forward, out hit, raycastDistance))
+        {
+            Debug.Log("Hit: " + hit.collider.gameObject.name);
+            gridObject = hit.collider.gameObject.GetComponent<GridObject>();
+
+            if (gridObject != null)
+            {
+                // Check if magnet button is pressed
+                
+                if (isMagnetActivated)
+                {
+                    gridObject.MoveToCell(gridObject.currentCell + getControllerDirection());
+                }
+            }
+
+         }
+    }
+
+    // Get direction from controller
+    Vector3Int getControllerDirection()
+    {
+        // Use controller rayOrigin forward direction 
+        Vector3 forward = rayOrigin.forward;
+
+        // Get primary direction
+        float absX = Mathf.Abs(forward.x);
+        float absY = Mathf.Abs(forward.y);
+        float absZ = Mathf.Abs(forward.z);
+
+        // Return direction as Vector3Int
+        if (absX > absY && absX > absZ)
+        {
+            return new Vector3Int(forward.x > 0 ? 1 : -1, 0, 0);
+        }
+        else if (absY > absX && absY > absZ)
+        {
+            return new Vector3Int(0, forward.y > 0 ? 1 : -1, 0);
+        }
+        else
+        {
+            return new Vector3Int(0, 0, forward.z > 0 ? 1 : -1);
+        }
+
+
+    }
+
+    // Test raycasrt
+    void OnDrawGizmos()
+    {
         if (xrController)
         {
-           
-            Vector3Int targetCell = gridObject.currentCell;
-            targetCell += Vector3Int.forward;
-            gridObject.MoveToCell(targetCell);
-        
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(rayOrigin.position, rayOrigin.forward * raycastDistance);
         }
     }
+
 
 
 }
