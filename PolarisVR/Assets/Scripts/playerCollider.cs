e28135a8b613b0d87967635fc74fa6693c58a52d
JamHam04 <james.hampson2004@gmail.com>
2026-01-10 17:30:30 +0000

    Door and Wall Models
    New Wall Tile models, including base tiles, and both inner and outer corner tiles.
    New Door model with separate door panels, frame, and wall tunnel tile

---
+++ PolarisVR/Assets/Scripts/playerCollider.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollider : MonoBehaviour
+{
+
+    public Transform headsetPosition;
+
+    public float padding = 0.3f;
+    public float minHeight = 0.5f;
+    public float maxHeight = 2.2f;
+
+    public float radiusFactor = 0.25f;
+
+    private CharacterController characterController;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+
+        if (headsetPosition == null && Camera.main != null)
+        {
+            headsetPosition = Camera.main.transform;
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        if (headsetPosition == null) return;
+        UpdateCapsule();
+    }
+
+    void UpdateCapsule()
+    {
+        // Y position of headset
+        float headsetY = headsetPosition.position.y - transform.position.y;
+
+        // Height calculation
+        float height = Mathf.Clamp(headsetY + padding, minHeight, maxHeight); // Clamp to prevent going too low/high
+
+        characterController.height = height; // Set height
+        characterController.radius = height * radiusFactor; // Set radius
+
+        // Center calculation
+        Vector3 localHeadOffset = transform.InverseTransformPoint(headsetPosition.position);
+
+        characterController.center = new Vector3(localHeadOffset.x, height * 0.5f, localHeadOffset.z); // Set center
+    }
+}
