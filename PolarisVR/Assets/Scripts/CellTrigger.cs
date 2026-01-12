e8b2c34d7f4d591381f29e4f0378ae8802453a01
JamHam04 <james.hampson2004@gmail.com>
2026-01-12 19:46:04 +0000

    Door Opening Animation
    Animate door open and close when triggers activated

--- PolarisVR/Assets/Scripts/CellTrigger.cs
+++ PolarisVR/Assets/Scripts/CellTrigger.cs
@@ -21,6 +21,7 @@ public class CellTrigger : MonoBehaviour
     // Trigger type
     public CubeType triggerType;
 
+
     // Start is called before the first frame update
     void Start()
     {
--- PolarisVR/Assets/Scripts/Door.cs
+++ PolarisVR/Assets/Scripts/Door.cs
@@ -11,9 +11,36 @@ public class Door : MonoBehaviour
     // Triggers needed to open/close door
     public List<CellTrigger> linkedTriggers = new List<CellTrigger>(); 
 
+    // Door collider
+    private BoxCollider doorCollider;
+
+    // Door panels
+    public GameObject leftDoorPanel;
+    public GameObject rightDoorPanel;
+    public GameObject bottomDoorPanel;
+
+
+    private Vector3 leftPanelStartPos;
+    private Vector3 rightPanelStartPos;
+    private Vector3 bottomPanelStartPos;
+
+    public float openSpeed = 2.0f;
+
+    void Start()
+    {
+        // Store initial positions
+        leftPanelStartPos = leftDoorPanel.transform.position;
+        rightPanelStartPos = rightDoorPanel.transform.position;
+        bottomPanelStartPos = bottomDoorPanel.transform.position;
+
+        // Get door collider
+        doorCollider = GetComponent<BoxCollider>();
+    }
+
     void Update()
     {
         CheckTriggers();
+        AnimateDoor();
     }
 
     // Check if all linked triggers are active
@@ -35,10 +62,8 @@ public class Door : MonoBehaviour
         if (!isOpen)
         {
             isOpen = true;
-            //gameObject.SetActive(false);
-            //doorCollider.enabled = false;
-            // Animate door opening
-
+            if (doorCollider != null)
+                doorCollider.enabled = false;
         }
     }
     public void CloseDoor()
@@ -46,8 +71,31 @@ public class Door : MonoBehaviour
         if (isOpen)
         {
             isOpen = false;
-            //doorCollider.enabled = true;
-            //gameObject.SetActive(true);
+            if (doorCollider != null)
+                doorCollider.enabled = true;
         }
     }
+
+    void AnimateDoor()
+    {
+        // Target panel positions
+        Vector3 leftTargetPos = leftPanelStartPos + new Vector3(-1.0f, 0.5f, 0);
+        Vector3 rightTargetPos = rightPanelStartPos + new Vector3(1.0f, 0.5f, 0);
+        Vector3 bottomTargetPos = bottomPanelStartPos + new Vector3(0, -1.0f, 0);
+
+        // Smoothly move panels
+        if (isOpen)
+        {
+            leftDoorPanel.transform.position = Vector3.Lerp(leftDoorPanel.transform.position, leftTargetPos, Time.deltaTime * openSpeed);
+            rightDoorPanel.transform.position = Vector3.Lerp(rightDoorPanel.transform.position, rightTargetPos, Time.deltaTime * openSpeed);
+            bottomDoorPanel.transform.position = Vector3.Lerp(bottomDoorPanel.transform.position, bottomTargetPos, Time.deltaTime * openSpeed);
+        }
+        else
+        {
+            leftDoorPanel.transform.position = Vector3.Lerp(leftDoorPanel.transform.position, leftPanelStartPos, Time.deltaTime * openSpeed);
+            rightDoorPanel.transform.position = Vector3.Lerp(rightDoorPanel.transform.position, rightPanelStartPos, Time.deltaTime * openSpeed);
+            bottomDoorPanel.transform.position = Vector3.Lerp(bottomDoorPanel.transform.position, bottomPanelStartPos, Time.deltaTime * openSpeed);
+        }
+    }
+
 }
